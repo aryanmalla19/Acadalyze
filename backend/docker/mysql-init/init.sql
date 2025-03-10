d6a30aa07f551d3a398d632ff71c12b48d7782af
Aryan Malla <aryanmalla19@gmail.com>
2025-03-10 15:46:00 +0545

    Added Database Base Schema!

--- backend/docker/mysql-init/init.sql
+++ backend/docker/mysql-init/init.sql
@@ -1,13 +1,12 @@
--- Create Tables
 CREATE TABLE roles (
     role_id INT AUTO_INCREMENT PRIMARY KEY,
-    role_name VARCHAR(50) NOT NULL
+    role_name VARCHAR(50) NOT NULL UNIQUE
 );
 
 CREATE TABLE schools (
     school_id INT AUTO_INCREMENT PRIMARY KEY,
     school_name VARCHAR(100) NOT NULL,
-    school_email VARCHAR(100) NOT NULL,
+    school_email VARCHAR(100) NOT NULL UNIQUE,
     established_date DATE NOT NULL,
     telephone_number VARCHAR(15) NOT NULL,
     address VARCHAR(255)
@@ -19,7 +18,7 @@ CREATE TABLE users (
     school_id INT NULL,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     address VARCHAR(255),
@@ -27,9 +26,72 @@ CREATE TABLE users (
     parent_phone_number VARCHAR(15),
     date_of_birth DATE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (role_id) REFERENCES roles(role_id),
-    FOREIGN KEY (school_id) REFERENCES schools(school_id)
+    FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE SET NULL,
+    FOREIGN KEY (school_id) REFERENCES schools(school_id) ON DELETE SET NULL
 );
+
+CREATE TABLE classes (
+    class_id INT AUTO_INCREMENT PRIMARY KEY,
+    class_teacher_id INT NOT NULL,
+    school_id INT NOT NULL,
+    class_name VARCHAR(20) NOT NULL,
+    FOREIGN KEY (class_teacher_id) REFERENCES users(user_id) ON DELETE RESTRICT,
+    FOREIGN KEY (school_id) REFERENCES schools(school_id) ON DELETE RESTRICT
+);
+
+CREATE TABLE subjects (
+    subject_id INT AUTO_INCREMENT PRIMARY KEY,
+    class_id INT NOT NULL,
+    subject_name VARCHAR(50) NOT NULL,
+    FOREIGN KEY (class_id) REFERENCES classes(class_id) ON DELETE CASCADE
+);
+
+CREATE TABLE exams (
+    exam_id INT AUTO_INCREMENT PRIMARY KEY,
+    exam_name VARCHAR(50) NOT NULL,
+    exam_date DATE NOT NULL
+);
+
+CREATE TABLE subjects_exams (
+    subjects_exams_id INT AUTO_INCREMENT PRIMARY KEY,
+    subject_id INT NOT NULL,
+    exam_id INT NOT NULL,
+    subject_exam_time TIMESTAMP,
+    pass_marks INT NOT NULL
+    full_marks INT NOT NULL
+    FOREIGN KEY (subject_id) REFERENCES subjects(subject_id) ON DELETE CASCADE,
+    FOREIGN KEY (exam_id) REFERENCES exams(exam_id) ON DELETE CASCADE
+);
+
+CREATE TABLE marks (
+    marks_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT NOT NULL,
+    subjects_exams_id INT NOT NULL,
+    marks_obtained INT NOT NULL
+    FOREIGN KEY (student_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (subjects_exams_id) REFERENCES subjects_exams(subjects_exams_id) ON DELETE CASCADE
+);
+
+CREATE TABLE attendance (
+    attendance_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT NOT NULL,
+    class_id INT NOT NULL,
+    attendance_date DATE NOT NULL,
+    status ENUM('Present', 'Absent', 'Late') NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (class_id) REFERENCES classes(class_id) ON DELETE CASCADE
+);
+
+CREATE TABLE student_classes (
+    student_class_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT NOT NULL,
+    class_id INT NOT NULL,
+    enrollment_date DATE NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (class_id) REFERENCES classes(class_id) ON DELETE CASCADE,
+    UNIQUE (student_id, class_id)
+);
+
 
 -- Insert Sample Data
 INSERT INTO roles (role_name) VALUES
