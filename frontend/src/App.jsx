e3fe55b54e7d85933dca6a3a626fd8312b04c6d9
Yudip Rai <yudip11@gmail.com>
2025-03-12 12:21:01 +0545

    feat(axios): add checkAuth function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,25 @@ import React, { useContext, useEffect } from "react";
 
 import { Route, Routes } from "react-router-dom";
 
-import Home from "./Pages/Home";
 import Page1 from "./Pages/Page1";
 import Page2 from "./Pages/Page2";
 import Page3 from "./Pages/Page3";
 import Page4 from "./Pages/Page4";
-
 import Sidebar from "./Components/Sidebar/Sidebar";
+
+import Home from "./Pages/Home";
 import Auth from "./Pages/Auth";
 import MainLayout from "./Layout/MainLayout";
 import { AuthContext } from "./Context/AuthContext";
+import { checkAuth } from "./Api/Api";
 
 const App = () => {
+  const { authUser, setAuthUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
   return (
     <div>
       <Routes>
